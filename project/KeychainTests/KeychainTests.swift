da16b13c1104dfab707318f6f091275a69e929cc
Douglas Bumby <douglas.bumby@icloud.com>
2015-06-08 00:15:25 -0700

    Add Carthage support

---
+++ project/KeychainTests/KeychainTests.swift
@@ -0,0 +1,36 @@
+//
+//  KeychainTests.swift
+//  KeychainTests
+//
+//  Created by Douglas Bumby on 2015-06-08.
+//  Copyright (c) 2015 Cosmic Labs. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class KeychainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
