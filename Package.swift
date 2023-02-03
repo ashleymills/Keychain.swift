f95296db935efaae15fd90908c743b645e31ffa5
Ashley Mills <ashleymills@mac.com>
2023-02-03 13:18:23 +0000

    Include macos

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Simple-KeychainSwift",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v11),
+        .macOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
